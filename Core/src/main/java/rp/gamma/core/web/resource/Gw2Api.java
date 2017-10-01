b3ead4b5a0314e26dc5b98012f1fe22c41111ab3
KevinGG <kawaigin@gmail.com>
2017-10-01 15:40:59 -0700

    Fix and IDE migration
    1. Getting rid of eclipse plugin from the Gradle project;
    2. Removing non utf-8 char from files;
    3. Updating gitignore to ignore Intellij files;
    4. Adding execution permission to gradlew.

--- Core/src/main/java/rp/gamma/core/web/resource/Gw2Api.java
+++ Core/src/main/java/rp/gamma/core/web/resource/Gw2Api.java
@@ -4,7 +4,7 @@ public enum Gw2Api {
   /**
    * @see <a href="https://wiki.guildwars2.com/wiki/API:2/commerce/listings">/v2/commerce/listings</a>
    * Parameters: 
-   * ids (optional list of numbers) – A comma-separated list of item ids to query the listings for.
+   * ids (optional list of numbers) A comma-separated list of item ids to query the listings for.
    * If it is accessed without specifying an id, a list of all ids is returned.
    * If id is provided, it will return all the buy/sell lists of the item with that id.
    */
@@ -13,7 +13,7 @@ public enum Gw2Api {
   /**
    * @see <a href="https://wiki.guildwars2.com/wiki/API:2/commerce/prices">/v2/commerce/prices</a>
    * Parameters: 
-   * ids (optional list of numbers) – A comma-separated list of item ids to query the listings for.
+   * ids (optional list of numbers) A comma-separated list of item ids to query the listings for.
    * If it is accessed without specifying an id, a list of all ids is returned.
    * If id is provided, it will only return the highest buy and lowest sell of the item with that id.
    */
@@ -22,7 +22,7 @@ public enum Gw2Api {
   /**
    * @see <a href="https://wiki.guildwars2.com/wiki/API:2/items">/v2/items</a>
    * Parameters:
-   * lang – (Optional) Request localized information.
+   * lang (Optional) Request localized information.
    * ids - (Optional; Comma Delimited List) Request an array of items for the specified ids. Cannot be used when using the id endpoint.
    */
   ITEM_DETAILS("/v2/items")
